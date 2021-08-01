86a5d8239b199ba91e6605f171b9768ded084b9c
mmilton <murray.l.milton@outlook.com>
2021-08-01 19:48:12 +0200

    made revisions to views to resolve postman errors

--- flashcardapp/flashcard_app/urls.py
+++ flashcardapp/flashcard_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cards/collections', views.CollectionList.as_view()),
-    path('cards/collections/details/<int:pk>', views.CardCollectionDetail.as_view()),
-    path('cards/details/<int:fk>', views.CardsInCollection.as_view()),
-    path('cards/editcard/<int:pk>', views.EditCard.as_view())
+    path('collections/', views.CollectionList.as_view()), # Will return all/Post Collections
+    path('collection/card/', views.CardList.as_view()), # Will return all cards
+    path('collection/card/<int:collection>/new/', views.CardList.as_view()),  # Will post Card by Collection ID
+    path('collection/card/<int:collection>/', views.CardDetail.as_view()),  # Will get card collection  by Collection ID
+    path('collection/card/<int:pk>/update/', views.CardEdit.as_view()),  # Will place the card Card PUT
+    path('collection/card/<int:pk>/delete/', views.CardEdit.as_view()),  # Will delete the Card DELETE
 ]
--- flashcardapp/flashcard_app/views.py
+++ flashcardapp/flashcard_app/views.py
@@ -8,67 +8,72 @@ from rest_framework import status
 
 class CollectionList(APIView):
 
-
     def get(self, request):
-        collection = Collections.objects.all()
-        serializer = CollectionSerializer(collection, many=True)
-        return Response(serializer.data)
-
+        try:
+            collection = Collections.objects.all()
+            serializer = CollectionSerializer(collection, many=True)
+            return Response(serializer.data)
+        except Collections.DoesNotExist:
+            raise Http404
 
     def post(self, request):
         serializer = CollectionSerializer(data=request.data)
         if serializer.is_valid():
-             serializer.save()
-             return Response(serializer.data, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class CardList(APIView):
 
-class CardCollectionDetail(APIView):
-
-    def get_object(self, pk):
+    def get(self, request):
         try:
-            return Collections.objects.get(id=pk)
+            card = Cards.objects.all()
+            serializer = CardSerializer(card, many=True)
+            return Response(serializer.data)
+        except Cards.DoesNotExist:
+            raise Http404
+
+    def post(self, request, collection):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CardDetail(APIView):
+
+    def get_object(self, collection):
+        try:
+            return Cards.objects.filter(collection=collection)
         except Collections.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        collection = self.get_object(pk)
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def delete(self, request, pk):
-        collection = self.get_object(pk)
-        serializer = CollectionSerializer(collection)
-        collection.delete()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, collection):
+        try:
+            card = self.get_object(collection)
+            serializer = CardSerializer(card, many=True)
+            return Response(serializer.data)
+        except Collections.DoesNotExist:
+            raise Http404
 
 
-class CardsInCollection(APIView):
-
-    def get(self, request, fk):
-        card = Cards.objects.filter(collection_id=fk)
-        serializer = CardSerializer(card, many=True)
-        return Response(serializer.data)
-
-
-class EditCard(APIView):
+class CardEdit(APIView):
 
     def get_object(self, pk):
         try:
-            return Cards.objects.get(id=pk)
+            return Cards.objects.get(pk=pk)
         except Cards.DoesNotExist:
             raise Http404
 
     def put(self, request, pk):
-        card_id = self.get_object(pk)
-        serializer = CardSerializer(card_id, data=request.data)
+        card = self.get_object(pk)
+        serializer = CardSerializer(card, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        card_id = self.get_object(pk)
-        serializer = CardSerializer(card_id)
-        card_id.delete()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        card = self.get_object(pk)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

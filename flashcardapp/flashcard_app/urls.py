532b89bb11db1743bb2863c9f1002f46733bc040
mmilton <murray.l.milton@outlook.com>
2021-08-02 07:08:22 +0200

    made revisions to url pattern to reslove postman error

--- flashcardapp/flashcard_app/urls.py
+++ flashcardapp/flashcard_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('collection/card/', views.CardList.as_view()), # Will return all cards
     path('collection/card/<int:collection>/new/', views.CardList.as_view()),  # Will post Card by Collection ID
     path('collection/card/<int:collection>/', views.CardDetail.as_view()),  # Will get card collection  by Collection ID
-    path('collection/card/<int:pk>/update/', views.CardEdit.as_view()),  # Will place the card Card PUT
+    path('collection/card/<int:pk>/editCard/', views.CardEdit.as_view()),  # Will place the card Card PUT
     path('collection/card/<int:pk>/delete/', views.CardEdit.as_view()),  # Will delete the Card DELETE
 ]
